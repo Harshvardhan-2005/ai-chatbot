27a8fe70d52c7b0091be2dc9e801749b372dedc5
Harshvardhan <harshvardhankarya@gmail.com>
2026-08-22 21:13:18 +0530

    fix settings configuration

--- app/core/config.py
+++ app/core/config.py
@@ -3,15 +3,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    GROQ_API_KEY: str
 
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     DEBUG: bool = True
-
-    GROQ_API_KEY: str
-    OPENAI_API_KEY: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
